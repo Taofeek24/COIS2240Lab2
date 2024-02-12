a2ab723ebb54d4774c6af8f8cf4ae14f152d96ac
Taofeek24 <159836683+Taofeek24@users.noreply.github.com>
2024-02-12 18:29:02 -0500

    Code Changes

--- Program.java
+++ Program.java
@@ -5,7 +5,7 @@ public class Program {
         ContactManager manager = new ContactManager();
 
         // Add contacts
-        manager.addContact("new Contact" ("Alaadin", "alaadin@trentu.ca", "123-456-7890"));
+        manager.addContact ("new Contact", ("Alaadin", "alaadin@trentu.ca", "123-456-7890"));
         manager.addContact("new Contact",("Bob", "bob@example.com", "098-765-4321"));
 
         // List contacts
